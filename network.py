93d568de9cdfa6983b499c9a72ac32b106b111f2
caoscott <scottcao@outlook.com>
2019-10-21 23:45:15 -0500

    Increase number of channels to see if msssim will improve

--- network.py
+++ network.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 # full assembly of the sub-parts to form the complete net
 
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from network_parts import double_conv, down, inconv, outconv, up, upconv
 
@@ -12,10 +10,10 @@ class Encoder(nn.Module):
     def __init__(self, channels_in: int, channels_out: int):
         super(Encoder, self).__init__()
         self.inc = inconv(channels_in, 64)
-        self.down1 = down(64, channels_out)
-        self.down2 = down(channels_out, channels_out)
-        self.down3 = down(channels_out, channels_out)
-        self.down4 = down(channels_out, channels_out)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 256)
+        self.down4 = down(256, 256)
         self.tanh = nn.Tanh()
 
     def forward(self, x: nn.Module) -> nn.Module:
@@ -31,9 +29,9 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, channels_in: int, channels_out: int):
         super(Decoder, self).__init__()
-        self.up1 = upconv(channels_in, channels_in, bilinear=False)
-        self.up2 = upconv(channels_in, channels_in, bilinear=False)
-        self.up3 = upconv(channels_in, 64, bilinear=False)
+        self.up1 = upconv(256, 256, bilinear=False)
+        self.up2 = upconv(256, 128, bilinear=False)
+        self.up3 = upconv(128, 64, bilinear=False)
         self.tanh = nn.Tanh()
         self.flow = upconv(64, 2, bilinear=False)
         self.residual = upconv(64, channels_out, bilinear=False)
@@ -43,6 +41,5 @@ class Decoder(nn.Module):
         x = self.up2(x)
         x = self.up3(x)
         f = self.tanh(self.flow(x).permute(0, 2, 3, 1))
-        # TODO: tanh?
         r = self.tanh(self.residual(x))
         return f, r
