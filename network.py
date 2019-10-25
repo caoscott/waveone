43a908896837b628cdc7bb22ac1a4e5168d5160b
caoscott <scottcao@outlook.com>
2019-10-25 01:00:39 -0500

    Added 1 more conv and deconv layer

--- network.py
+++ network.py
@@ -11,22 +11,17 @@ from network_parts import double_conv, down, inconv, outconv, up, upconv
 class Encoder(nn.Module):
     def __init__(self, channels_in: int, channels_out: int):
         super(Encoder, self).__init__()
-        self.inc = inconv(channels_in, 64)
-        self.down1 = down(64, 128)
-        self.down2 = down(128, 128)
-        self.down3 = down(128, 128)
-        self.down4 = down(128, 128)
-        self.tanh = nn.Tanh()
+        self.encode = nn.Sequential(
+            inconv(channels_in, 64),
+            down(64, 128),
+            down(128, 128),
+            down(128, 128),
+            down(128, 128),
+            down(128, 128),
+            nn.Tanh())
 
     def forward(self, x: nn.Module) -> nn.Module:
-        x = self.inc(x)
-        x = self.down1(x)
-        x = self.down2(x)
-        x = self.down3(x)
-        x = self.down4(x)
-        x = self.tanh(x)
-        print(x.shape)
-        return x
+        return self.encode(x)
 
 
 class Decoder(nn.Module):
@@ -34,23 +29,24 @@ class Decoder(nn.Module):
 
     def __init__(self, channels_in: int, channels_out: int):
         super(Decoder, self).__init__()
-        self.up1 = upconv(128, 128, bilinear=False)
-        self.up2 = upconv(128, 128, bilinear=False)
-        self.up_flow = upconv(128, 64, bilinear=False)
-        self.up_residual = upconv(128, 64, bilinear=False)
-        self.tanh = nn.Tanh()
-        self.sigmoid = nn.Sigmoid()
-        self.flow = upconv(64, 2, bilinear=False)
-        self.residual = upconv(64, channels_out, bilinear=False)
+        self.ups = nn.Sequential(
+            upconv(128, 128, bilinear=False),
+            upconv(128, 128, bilinear=False),
+            upconv(128, 128, bilinear=False))
+        self.flow = nn.Sequential(
+            upconv(128, 64, bilinear=False),
+            upconv(64, 2, bilinear=False),
+            nn.Tanh())
+        self.residual = nn.Sequential(
+            upconv(128, 64, bilinear=False),
+            upconv(64, channels_out, bilinear=False),
+            nn.Tanh())
 
     def forward(self, x: nn.Module) -> nn.Module:
-        x = self.up1(x)
-        x = self.up2(x)
-        f = self.up_flow(x)
-        r = self.up_residual(x)
-        r = self.tanh(self.residual(r))
+        x = self.ups(x)
+        r = self.residual(x)
         identity_theta = torch.tensor(
             Decoder.IDENTITY_TRANSFORM * x.shape[0]).cuda()
-        f = self.tanh(self.flow(f).permute(0, 2, 3, 1)) + \
+        f = self.flow(x).permute(0, 2, 3, 1) + \
             F.affine_grid(identity_theta, r.shape)
         return f, r
