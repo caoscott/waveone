6fe0988cf278579a7d74a6a07812e308ff859673
caoscott <scottcao@outlook.com>
2019-10-29 19:12:27 -0500

    Added stuff to use context better

--- network.py
+++ network.py
@@ -24,9 +24,8 @@ class Encoder(nn.Module):
         self.use_context = use_context
 
     def forward(self, x: nn.Module, context_vec: torch.Tensor) -> nn.Module:
-        x = self.encode_frames(x)
-        if self.use_context:
-            x += self.encode_context(context_vec)
+        x = self.encode_frames(x) + self.encode_context(
+            context_vec) if self.use_context else 0.
         return self.encode(x)
 
 
