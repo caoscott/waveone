f8d42860d50bc208d858ed22ad76563716ac630d
caoscott <scottcao@outlook.com>
2019-11-08 12:49:09 -0600

    Changing frame_len back to 2

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def train():
         is_train=True,
         root=args.train,
         mv_dir=args.train_mv,
-        frame_len=5,
+        frame_len=2,
         args=args
     )
     eval_loaders = {
