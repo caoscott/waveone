6a0a3427ccaacc9673458c0d4177b06b30a87e0b
caoscott <scottcao@outlook.com>
2019-11-22 15:45:27 -0600

    Changing precision on cuda

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 from waveone.losses import MSSSIM
 from waveone.network_parts import LambdaModule
@@ -27,3 +28,13 @@ def test_forward_model_zero_residual():
     assert residuals.norm().item() == pytest.approx(0.)
     l2_score = nn.MSELoss()(reconstructed, frame).item()
     assert l2_score == pytest.approx(0.)
+
+
+def test_identity_flow():
+    for _ in range(500):
+        x = torch.rand((24, 3, 64, 64))
+        identity_theta = torch.tensor(
+            [[[1., 0., 0.], [0., 1., 0.]]] * x.shape[0])
+        f = F.affine_grid(identity_theta, x.shape, align_corners=False)
+        x2 = F.grid_sample(x, f, align_corners=False)
+        assert nn.MSELoss()(x, x2).item() <= 1e-10
