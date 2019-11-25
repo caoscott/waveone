369ae6071dd3a17d6b2d76b5f9ef706bff72cf4b
Sheng Cao <scottcao@cs.utexas.edu>
2019-11-25 14:47:49 -0600

    Change device settings

--- network.py
+++ network.py
@@ -101,10 +101,10 @@ class BitToFlowDecoder(nn.Module):
         x = self.ups(x)
         r = self.residual(x)  # .clamp(-1., 1.)
         identity_theta = torch.tensor(
-            BitToFlowDecoder.IDENTITY_TRANSFORM * x.shape[0]).cuda()
+            BitToFlowDecoder.IDENTITY_TRANSFORM * x.shape[0]).to(x.device)
         f = self.flow(x)
         grid_normalize = torch.tensor(
-            f.shape[2: ]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+            f.shape[2: ]).unsqueeze(0).unsqueeze(-1).unsqueeze(-1).to(x.device)
         f = (f / grid_normalize).permute(0, 2, 3, 1)
         f += F.affine_grid(identity_theta, r.shape, align_corners=False)  # type: ignore
         # f = F.affine_grid(identity_theta, r.shape,  # type: ignore
