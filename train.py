94c15e872820d3496ff07a60f494f8cdf2e16351
caoscott <scottcao@outlook.com>
2019-12-07 03:48:43 -0600

    Revert "Removing save max code"
    This reverts commit 4c1dacb18d5e0e825aea41b6e4bef00249477f16.

--- train.py
+++ train.py
@@ -414,7 +414,20 @@ def train(args) -> nn.Module:
     for epoch in range(args.max_train_epochs):
         for frames in train_loader:
             train_iter += 1
-            train_loop(frames)
+            max_epoch_l2, max_epoch_l2_frames = max(
+                train_loop(frames), key=lambda x: x[0])
+
+        if args.save_max_l2:
+            save_tensor_as_img(
+                max_epoch_l2_frames[1],
+                f"{max_epoch_l2 :.6f}_{epoch}_max_l2_frame",
+                args,
+            )
+            save_tensor_as_img(
+                max_epoch_l2_frames[2],
+                f"{max_epoch_l2 :.6f}_{epoch}_max_l2_reconstructed",
+                args,
+            )
 
         if (epoch + 1) % args.checkpoint_epochs == 0:
             save(args, model)
