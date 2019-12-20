[FIX] move loss computation from train to network

network.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from torchvision import models
 
 from waveone.network_parts import (ConvLSTMCell, SatLU, Sign, down, inconv,
-                                   outconv, revnet_block, up, upconv)
+                                   outconv, revnet_block, up, upconv, LambdaModule)
 from collections import defaultdict
 
 
@@ -136,13 +136,18 @@ class WaveoneModel(nn.Module):
                  encoder: nn.Module,
                  binarizer: nn.Module,
                  decoder: nn.Module,
-                 train_type: str) -> None:
+                 train_type: str,
+                 flow_loss_fn: nn.Module,
+                 reconstructed_loss_fn: nn.Module
+    ) -> None:
         super().__init__()
         self.encoder = encoder
         self.binarizer = binarizer
         self.decoder = decoder
         self.train_type = train_type
         self.nets = (encoder, binarizer, decoder)
+        self.flow_loss_fn = flow_loss_fn
+        self.reconstructed_loss_fn = reconstructed_loss_fn
 
     def forward(  # type: ignore
             self,
@@ -153,8 +158,9 @@ class WaveoneModel(nn.Module):
     ) -> Dict[str, torch.Tensor]:
         device = next(self.parameters()).device
         frame1 = frames[0].to(device)
+        loss: torch.Tensor = 0.
         out_collector: DefaultDict[str, List[torch.Tensor]] = defaultdict(list)
-        for iter_i, frame2 in enumerate(frames[1:]):  # type: ignore  
+        for iter_i, frame2 in enumerate(frames[1:]):  # type: ignore
             frame2: torch.Tensor = frame2.to(device)  # type: ignore
             codes = self.binarizer(self.encoder(frame1, frame2, 0.))
             decoder_out = self.decoder((codes, 0.))
@@ -177,13 +183,19 @@ class WaveoneModel(nn.Module):
                     reconstructed_frame2, min=-1., max=1.)
             out_collector["reconstructed_frame2"].append(reconstructed_frame2)
 
+            loss += self.flow_loss_fn(frame2, decoder_out["flow_frame2"])
+            loss += self.reconstructed_loss_fn(
+                frame2, decoder_out["reconstructed_frame2"]
+            )
+
             frame1 = (reconstructed_frame2 if reuse_frame and
                       iter_i % iframe_iter != 0 else frame2)
             if detach:
                 frame1 = frame1.detach()
 
         return {
-            k: torch.stack(v) for k, v in out_collector.items()
+            **{k: torch.stack(v) for k, v in out_collector.items()},
+            **{"loss": loss / (frames.shape[0]-1)},
         }
 
 

train.py
@@ -193,6 +193,8 @@ def get_model(args: argparse.Namespace) -> nn.Module:
     #     binarizer = Binarizer(args.bits, args.bits,
     #                           not args.binarize_off)
     #     return WaveoneModel(encoder, binarizer, decoder, args.train_type)
+    flow_loss_fn = get_loss_fn(args.flow_loss).cuda()
+    reconstructed_loss_fn = get_loss_fn(args.reconstructed_loss).cuda()
     if args.network == "cae":
         return CAE()
     if args.network == "unet":
@@ -206,12 +208,18 @@ def get_model(args: argparse.Namespace) -> nn.Module:
             "residuals": t[0],
             "context_vec": torch.zeros(1),
         })
-        return WaveoneModel(opt_encoder, opt_binarizer, opt_decoder, train_type="residual")
+        return WaveoneModel(
+            opt_encoder, opt_binarizer, opt_decoder, "residual", 
+            flow_loss_fn, reconstructed_loss_fn,
+        )
     if args.network == "small":
         small_encoder = SmallEncoder(6, args.bits)
         small_binarizer = SmallBinarizer(not args.binarize_off)
         small_decoder = SmallDecoder(args.bits, 3)
-        return WaveoneModel(small_encoder, small_binarizer, small_decoder, args.train_type)
+        return WaveoneModel(
+            small_encoder, small_binarizer, small_decoder, args.train_type, 
+            flow_loss_fn, reconstructed_loss_fn,
+        )
     raise ValueError(f"No model type named {args.network}.")
 
 
@@ -240,8 +248,6 @@ def train(args) -> nn.Module:
         weight_decay=args.weight_decay
     )
     scheduler = LS.StepLR(solver, step_size=40, gamma=0.5)
-    reconstructed_loss_fn = get_loss_fn(args.reconstructed_loss).cuda()
-    flow_loss_fn = get_loss_fn(args.flow_loss).cuda()
     # tv = TotalVariation().cuda()
 
     def log_flow_context_residuals(
@@ -294,8 +300,6 @@ def train(args) -> nn.Module:
         model_out = model(
             frames, iframe_iter=sys.maxsize, reuse_frame=True, detach=args.detach
         )
-        loss = (flow_loss_fn(frames[1:], model_out["flow_frame2"])
-                + reconstructed_loss_fn(frames[1:], model_out["reconstructed_frame2"]))
 
         log_flow_context_residuals(
             writer,
@@ -308,7 +312,7 @@ def train(args) -> nn.Module:
         #     frame1 = frame1.detach()
 
         if args.network != "opt":
-            loss.backward()
+            model_out["loss"].backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
             solver.step()
 
@@ -323,9 +327,7 @@ def train(args) -> nn.Module:
                               "train_reconstructed"),
             }
             writer.add_scalar(
-                "training_loss",
-                loss.item(),
-                train_iter,
+                "training_loss", model_out["loss"].item(), train_iter,
             )
             writer.add_scalar(
                 "lr", solver.param_groups[0]["lr"], train_iter)  # type: ignore