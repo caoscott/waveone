8d74770cd12f4e16b7477aa476da714cb56a8120
tomgreen <scottcao@outlook.com>
2019-12-20 13:51:18 -0600

    Add type: ignore to enumerate(torch.tensor)

--- network.py
+++ network.py
@@ -154,9 +154,8 @@ class WaveoneModel(nn.Module):
         device = next(self.parameters()).device
         frame1 = frames[0].to(device)
         out_collector: DefaultDict[str, List[torch.Tensor]] = defaultdict(list)
-        frame2: torch.Tensor
-        for iter_i, frame2 in enumerate(frames[1:]):  # type: ignore
-            frame2 = frame2.to(device)
+        for iter_i, frame2 in enumerate(frames[1:]):  # type: ignore  
+            frame2: torch.Tensor = frame2.to(device)  # type: ignore
             codes = self.binarizer(self.encoder(frame1, frame2, 0.))
             decoder_out = self.decoder((codes, 0.))
             for k, v in decoder_out.items():
